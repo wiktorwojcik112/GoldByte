25df3d264c79d08d531a9eb2d6fffc379b7647a9
Wiktor Wójcik <wiktorwojcik112@gmail.com>
2021-12-08 17:36:02 +0100

    Improvements for Linux

--- Sources/GoldByte/resource-accesor.swift
+++ Sources/GoldByte/resource-accesor.swift
@@ -3,7 +3,7 @@ import class Foundation.Bundle
 private class BundleFinder {}
 
 extension Foundation.Bundle {
-	static var currentModule: Bundle = {
+	static var macModule: Bundle = {
 		let bundleName = "GoldByte_GoldByte"
 		
 		let candidates = [
@@ -24,5 +24,21 @@ extension Foundation.Bundle {
 			}
 		}
 		fatalError("unable to find bundle named GoldByte_GoldByte")
+	}()
+	
+	static var linuxModule: Bundle? = {
+		#if os(Linux)
+		return .module
+		#else
+		return nil
+		#endif
+	}()
+	
+	static var currentModule: Bundle = {
+		#if os(macOS)
+		return .macModule
+		#elseif os(Linux)
+		return .linuxModule!
+		#endif
 	}()
 }
